aea443eb843355a1fa418f539492ba1e8f88e0ab
lbenz730 <lukesbenz@gmail.com>
2025-04-29 08:40:07 -0400

    replace old file

--- scripts/simulations/missing_data/analyze_bootstrap_variance.R
+++ scripts/simulations/missing_data/analyze_bootstrap_variance.R
@@ -21,22 +21,40 @@ df_contain <-
   group_by(sim_id, dataset_id) %>% 
   summarise('n_boot' = n(),
             'truth' = mean(true_ate),
-            'lower' = quantile(estimate, 0.025, na.rm = T),
             'mean' = mean(estimate, na.rm = T),
             'median' = median(estimate, na.rm = T),
-            'upper' = quantile(estimate, 0.975, na.rm = T)) %>% 
+            
+            ### Percentile Method
+            'lower_percentile' = quantile(estimate, 0.025, na.rm = T),
+            'upper_percentile' = quantile(estimate, 0.975, na.rm = T),
+            
+            ### Normal Approximation
+            'lower_normal' = mean(estimate, na.rm = T) + qnorm(0.025) * sd(estimate, na.rm = T),
+            'upper_normal' = mean(estimate, na.rm = T) + qnorm(0.975) * sd(estimate, na.rm = T),
+            
+            ### Emprirical Method
+            'lower_pivot' = 2 * mean(estimate, na.rm = T) - quantile(estimate, 0.975, na.rm = T),
+            'upper_pivot' = 2 * mean(estimate, na.rm = T) - quantile(estimate, 0.025, na.rm = T),
+            
+            ) %>% 
   ungroup() %>% 
-  mutate('interval_contain' = truth >= lower & truth <= upper)
-
+  mutate('interval_contain_percentile' = truth >= lower_percentile & truth <= upper_percentile,
+         'interval_contain_normal' = truth >= lower_normal & truth <= upper_normal,
+         'interval_contain_pivot' = truth >= lower_pivot & truth <= upper_pivot)
+         
 df_contain %>% 
   group_by(sim_id) %>% 
-  summarise('coverage' = mean(interval_contain),
+  summarise(
+    'coverage_normal' = mean(interval_contain_normal),
+    'coverage_pivot' = mean(interval_contain_pivot),
+    'coverage_percentile' = mean(interval_contain_percentile),
+           
             'n_boot' = min(n_boot),
             'mean' = mean(mean),
             'ate' = mean(truth)) %>% 
   mutate('setting' = case_when(sim_id == 13 ~ 'M-Bias',
                                sim_id == 14 ~ 'Treatment Effect Heterogeneity',
                                sim_id == 15 ~ 'M-Bias w/ Mediator')) %>% 
-  select(setting, coverage) %>% 
+  select(setting, contains('coverage')) %>% 
   kable(align = 'c', format = 'latex')
 
